17214e4ff8d9ca8b51b7581909b05f5683eeeb17
shreyas24 <shreyaspai.24@gmail.com>
2013-07-17 22:51:31 +0530

    Update as per HW4

--- web.js
+++ web.js
@@ -2,26 +2,14 @@ var express = require('express'),
 	fs = require('fs'),
 	path = require('path');
 
-var app = express.createServer(express.logger('dev')),
-	filename = './public/index.html';
-	
-app.use(express.static(path.join('./public')));
+var app = express.createServer();
 
+var WORKING_DIR = './public/';
 
-app.get('/', function(request, response) {
-	fs.readFile(filename, function (err, html) {
-		if(err)
-		{	response.setHeader("Content-Type", "text/plain");
-			response.write("404 Not found\n");
-			response.end("There is some error, that's all we know");
-		}
-		else{
-			response.setHeader("Content-Type", "text/html");
-			response.setHeader("Content-Length", html.length);
-			response.write(html);
-		}
-	});
-});
+app.use(express.logger('dev'));
+app.use(express.favicon(WORKING_DIR + '/favicon.ico'));
+app.use(express.static(path.join(WORKING_DIR)));
+
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
